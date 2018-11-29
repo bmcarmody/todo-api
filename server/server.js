6af1224402444fcd025049141e5b88761342666f
Brandon <BmcPrimary@yahoo.com>
2018-11-28 21:56:29 -0500

    Add DELETE /users/me/token route

--- server/server.js
+++ server/server.js
@@ -125,6 +125,14 @@ app.post('/users/login', (req, res) => {
     });
 });
 
+app.delete('/users/me/token', authenticate, (req, res) => {
+    req.user.removeToken(req.token).then(() => {
+        res.status(200).send();
+    }, () => {
+        res.status(400).send();
+    })
+});
+
 //Listens on specific port which is determined at the beginning of the script
 app.listen(process.env.PORT, () => {
     console.log(`Started on port ${process.env.PORT}`);
