bd513d4c9b2f3b5c73371732b772fa10293be619
Brandon <BmcPrimary@yahoo.com>
2018-11-29 12:38:34 -0500

    Fixed Heroku issue

--- server/config/config.js
+++ server/config/config.js
@@ -1,7 +1,11 @@
 let env = process.env.NODE_ENV || 'development';
 
 if (env === 'development' || 'test') {
-    let config = require('./config.json');
+    try {
+        let config = require('./config.json'); 
+    } catch(e) {
+        return "";
+    }
     let envConfig = config[env];
     
     Object.keys(envConfig).forEach((key) => {
